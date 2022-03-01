63c768e8dd82efb849bc65d22f15bbba5d419612
congpeitong <congpeitong2022@163.com>
2022-03-01 10:33:16 +0800

    栈+哈希表解决下一个更大的数

--- src/main/java/malehunter/datastructure/leetcode/LeetCode496.java
+++ src/main/java/malehunter/datastructure/leetcode/LeetCode496.java
@@ -1,6 +1,4 @@
 package malehunter.datastructure.leetcode;
-
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Stack;
 
@@ -48,20 +46,37 @@ public class LeetCode496 {
         return result;
     }
 
-    @Deprecated
+    /**
+     * 栈+哈希表  哈希表内存储下一个更大的值  key：值  value:key的下一个更大的值（可以通过栈来计算下一个更大的值） 最后直接通过hashTable的key查询到它对应的下一个最大的值
+     * @param nums1
+     * @param nums2
+     * @return
+     */
     public static int[] nextGreaterElement2(int[] nums1, int[] nums2) {
+        int[] result = new int[nums1.length];
         HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = nums2.length - 1; i >= 0; i--) {
-            map.put(nums2[i], i);
+        Stack<Integer> stack = new Stack<>();
+        for (int num : nums2) {
+            while (stack.size()!=0 && num > stack.peek()) {
+                Integer pop = stack.pop();
+                map.put(pop,num);
+            }
+            stack.add(num);
         }
-
-        return null;
+        while (stack.size() != 0) {
+            Integer pop = stack.pop();
+            map.put(pop,-1);
+        }
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = map.get(nums1[i]);
+        }
+        return result;
     }
 
     public static void main(String[] args) {
         int[] num1 = {4,1,2};
         int[] num2 = {1,3,4,2};
-        int[] ints = nextGreaterElement1(num1, num2);
+        int[] ints = nextGreaterElement2(num1, num2);
         for (int anInt : ints) {
             System.out.println(anInt);
         }
